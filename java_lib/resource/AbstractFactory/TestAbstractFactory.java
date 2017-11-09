948e365df542be47dcd90d0cd3c240b5fa91c739
Darren Xie <mndarren@gmail.com>
2017-11-08 23:11:39 -0600

    format

--- java_lib/resource/AbstractFactory/TestAbstractFactory.java
+++ java_lib/resource/AbstractFactory/TestAbstractFactory.java
@@ -6,8 +6,16 @@ public class TestAbstractFactory {
 	}
 
 	private static void testAbstractFactory() {
-		Computer pc = ComputerFactory.INSTANCE.getComputer(new PCFactory.Builder().ram("2 GB").hdd("500 GB").cpu("2.4 GHz").build());
-		Computer server = ComputerFactory.INSTANCE.getComputer(new ServerFactory.Builder().ram("16 GB").hdd("1 TB").cpu("294 GHz").build());
+		Computer pc = ComputerFactory.INSTANCE.getComputer(new PCFactory.Builder()
+		                                                                .ram("2 GB")
+																		.hdd("500 GB")
+																		.cpu("2.4 GHz")
+																		.build());
+		Computer server = ComputerFactory.INSTANCE.getComputer(new ServerFactory.Builder()
+		                                                                        .ram("16 GB")
+																				.hdd("1 TB")
+																				.cpu("294 GHz")
+																				.build());
 		System.out.println("AbstractFactory PC Config: " + pc);
 		System.out.println("AbstractFactory Server Config: " + server);
 	}
