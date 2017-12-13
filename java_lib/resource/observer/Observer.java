4365697442f3cd7757a24c873c5c6fb134dc5547
Darren Xie <mndarren@gmail.com>
2017-12-13 11:29:06 -0600

    add back observer

---
+++ java_lib/resource/observer/Observer.java
@@ -0,0 +1,9 @@
+package observer;
+
+public interface Observer {
+	//method to update the observer, used by subject
+	public void update();
+	//attach with subject to observe
+	public void setSubject(Subject sub);
+
+}
