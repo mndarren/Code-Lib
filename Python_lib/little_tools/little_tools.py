84b29f15f6a9e06a1431a7e98551c90ef9414dfb
Darren Xie <mndarren@gmail.com>
2018-07-02 22:56:04 -0500

    updated python little tools

--- Python_lib/little_tools/little_tools.py
+++ Python_lib/little_tools/little_tools.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# created by Darren Xie
+# created by Darren Xie on June 2018
 """
 .. currentmodule:: little_tools
-.. moduleauthor:: Stearns Financial Center <SoftwareDevelopment@stearnsbank.com>
-
-Address Reconciler
+Address LittleTools
 """
 
 from typing import List
@@ -18,8 +16,8 @@ class LittleTools(object):
 
     @staticmethod
     def is_empty_string(string_to_check: str or None):
-        """"""
-        if string_to_check is None or len(string_to_check.strip()) == 0:
+        """is None, '', '  '"""
+        if not string_to_check or len(string_to_check.strip()) == 0:
             return True
         return False
 
@@ -79,3 +77,24 @@ class LittleTools(object):
             else:
                 new_list.extend(['\'', '\''])
         return ''.join(new_list)
+
+ 
+import cProfile, pstats, io
+
+class PerformanceChecker(object):
+    @staticmethod
+    def profile(fnc):
+        """A decorator that uses cProfile to profile a function"""
+        def inner(*args, **kwargs):
+            pr = cProfile.Profile()
+            pr.enable()
+            retval = fnc(*args, **kwargs)
+            pr.disable()
+            s = io.StringIO()
+            sortby = 'cumulative'
+            ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+            ps.print_stats()
+            print(s.getvalue())
+            return retval
+
+        return inner
