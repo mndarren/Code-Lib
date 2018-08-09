be12c8155d3b79b16d91c5b590c20bbbc2913335
Darren Xie <darrenx@stearnsbank.com>
2018-08-09 17:08:31 -0500

    updated drawing

--- Python_lib/coding_challenge/fun5.py
+++ Python_lib/coding_challenge/fun5.py
@@ -15,11 +15,35 @@ import turtle
 
 wife = turtle.Turtle()
 wife.pencolor("red")
-wife.left(90)
 
-for i in range(0, 360):
-    wife.forward(2)
-    wife.right(1)
+wife.penup()
+wife.goto(0,-40)
+wife.pendown()
+wife.circle(200)
+
+
+
+smiles = turtle.Turtle()    
+smiles.penup()
+smiles.goto(-75,150)
+smiles.pendown()
+smiles.circle(10)     #eye one
+
+smiles.penup()
+smiles.goto(75,150)
+smiles.pendown()
+smiles.circle(10)     #eye two
+
+smiles.penup()
+smiles.goto(0,0)
+smiles.pendown()
+smiles.circle(100,90)   #right smile
+
+smiles.penup()           
+smiles.setheading(180) # <-- look West
+smiles.goto(0,0)
+smiles.pendown()
+smiles.circle(-100,90)
 
 exit = input()
-wife.done()
+smiles.done()
