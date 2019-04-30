f94d7af9d603eae2d5392c9993699284ecddc601
Darren Xie <darrenx@stearnsbank.com>
2019-04-30 15:19:27 -0500

    sql function

---
+++ SQL_lib/examples/remove_special_char_from_table.sql
@@ -0,0 +1,45 @@
+DECLARE @col_name varchar(50)
+DECLARE @Q varchar(255)
+DECLARE Loan_Col_Cursor CURSOR FOR
+select C.name as column_name 
+from sys.all_columns C
+left join sys.tables T
+on C.object_id = t.object_id
+where t.name = 'loan_tbl'
+OPEN Loan_Col_Cursor
+FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @Q = 'UPDATE loan_tbl SET '+@col_name+' = REPLACE('+@col_name+', ''|'', '''')'
+		exec (@Q)
+		FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
+	END
+CLOSE Loan_Col_Cursor
+DEALLOCATE Loan_Col_Cursor
+GO
+
+--Function to remove all Pipe in a table
+DROP PROCEDURE IF EXISTS dbo.removePipeFromTable;
+GO
+CREATE PROCEDURE dbo.removePipeFromTable (@tbl_name VARCHAR) AS
+BEGIN
+DECLARE @col_name varchar(50)
+DECLARE @Q varchar(255)
+DECLARE Loan_Col_Cursor CURSOR FOR
+select C.name as column_name 
+from sys.all_columns C
+left join sys.tables T
+on C.object_id = t.object_id
+where t.name = @tbl_name
+OPEN Loan_Col_Cursor
+FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
+WHILE @@FETCH_STATUS = 0
+	BEGIN
+		SET @Q = 'UPDATE '+@tbl_name+' SET '+@col_name+' = REPLACE('+@col_name+', ''|'', '''')'
+		exec (@Q)
+		FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
+	END
+CLOSE Loan_Col_Cursor
+DEALLOCATE Loan_Col_Cursor
+END;
+GO
