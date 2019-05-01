489db63478aa1240bd66ee6a7f261a38649c4d28
Darren Xie <darrenx@stearnsbank.com>
2019-05-01 11:40:21 -0500

    update sql for better performance

--- SQL_lib/examples/remove_special_char_from_table.sql
+++ SQL_lib/examples/remove_special_char_from_table.sql
@@ -1,24 +1,4 @@
-DECLARE @col_name varchar(50)
-DECLARE @Q varchar(255)
-DECLARE Loan_Col_Cursor CURSOR FOR
-select C.name as column_name 
-from sys.all_columns C
-left join sys.tables T
-on C.object_id = t.object_id
-where t.name = 'loan_tbl'
-OPEN Loan_Col_Cursor
-FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
-WHILE @@FETCH_STATUS = 0
-	BEGIN
-		SET @Q = 'UPDATE loan_tbl SET '+@col_name+' = REPLACE('+@col_name+', ''|'', '''')'
-		exec (@Q)
-		FETCH NEXT FROM Loan_Col_Cursor INTO @col_name
-	END
-CLOSE Loan_Col_Cursor
-DEALLOCATE Loan_Col_Cursor
-GO
-
---Function to remove all Pipe in a table
+--Procedure to remove all Pipe in a table
 DROP PROCEDURE IF EXISTS dbo.removePipeFromTable;
 GO
 CREATE PROCEDURE dbo.removePipeFromTable (@tbl_name VARCHAR(50)) AS
@@ -41,5 +21,48 @@ WHILE @@FETCH_STATUS = 0
 	END
 CLOSE Loan_Col_Cursor
 DEALLOCATE Loan_Col_Cursor
+END
+GO
+
+--version 2: execute update queries once when query string length gets 1000 characters
+DROP PROCEDURE IF EXISTS dbo.removePipeFromTable;
+GO
+CREATE PROCEDURE dbo.removePipeFromTable (@tbl_name VARCHAR(50)) AS
+BEGIN
+DECLARE @col_name VARCHAR(50)
+DECLARE @temp_q VARCHAR(100)
+DECLARE @Q VARCHAR(1000) = 'UPDATE '+@tbl_name+' SET '
+--get table column name cursor
+DECLARE Table_Col_Cursor CURSOR FOR
+SELECT C.name AS column_name 
+FROM sys.all_columns C
+LEFT JOIN sys.tables T
+ON C.object_id = t.object_id
+WHERE t.name = @tbl_name
+
+OPEN Table_Col_Cursor
+FETCH NEXT FROM Table_Col_Cursor INTO @col_name
+--build update query string, execute once when length of string gets 1000 characters
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	SET @temp_q = @col_name+' = REPLACE('+@col_name+', ''|'', ''''),'
+	IF LEN(@Q + @temp_q) >= 1000
+		BEGIN
+			SET @Q = LEFT(@Q, LEN(@Q)-1)
+			EXEC (@Q)
+			SET @Q = 'UPDATE '+@tbl_name+' SET '
+		END
+	SET @Q = @Q + @temp_q
+	FETCH NEXT FROM Table_Col_Cursor INTO @col_name
+END
+--last execute the update query string
+IF LEN(@Q) > 0
+	BEGIN
+		SET @Q = LEFT(@Q, LEN(@Q)-1)
+		EXEC (@Q)
+	END
+
+CLOSE Table_Col_Cursor
+DEALLOCATE Table_Col_Cursor
 END
 GO
